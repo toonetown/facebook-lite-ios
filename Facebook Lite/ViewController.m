822bf70cc7da4d894db4dc09b7ab673ae5cca3fe
Nathan Toone <nathan@toonetown.com>
2018-01-11 07:37:32 -0700

    Tweak the bar color so it matches better

--- Facebook Lite/ViewController.m
+++ Facebook Lite/ViewController.m
@@ -14,7 +14,10 @@
 
 // The URL to load into the controller
 #define BASE_URL  [NSURL URLWithString:@"https://www.facebook.com"]
-#define BAR_COLOR [UIColor colorWithRed:0.23 green:0.35 blue:0.60 alpha:1.0]
+#define BAR_COLOR [UIColor colorWithRed:((27.0)/255.0) \
+                                  green:((62.0)/255.0) \
+                                   blue:((136.0)/255.0) \
+                                  alpha:1.0]
 
 @implementation ViewController
 
